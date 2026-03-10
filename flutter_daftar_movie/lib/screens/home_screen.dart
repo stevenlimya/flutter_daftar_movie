919ab655ae41def5d73c9bfd0b9431bec1d2676a
adhitya <umniyudaa@gmail.com>
2026-03-10 17:37:42 +0700

    finally

---
+++ flutter_daftar_movie/lib/screens/home_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_daftar_movie/models/movie.dart';
+import 'package:flutter_daftar_movie/services/api_services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiService _apiService = ApiService();
+
+  List<Movie> _allMovies = [];
+  List<Movie> _trendingMovies = [];
+  List<Movie> _popularMovies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMovies();
+  }
+
+  Future<void> _loadMovies() async {
+    final List<Map<String, dynamic>> allMoviesData = await _apiService
+        .getAllMovies();
+    final List<Map<String, dynamic>> trendingMoviesData = await _apiService
+        .getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData = await _apiService
+        .getPopularMovies();
+
+    setState(() {
+      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _trendingMovies = trendingMoviesData
+          .map((e) => Movie.fromJson(e))
+          .toList();
+      _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Film')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildMoviesList('All Movies', _allMovies),
+            _buildMoviesList('Trending Movies', _trendingMovies),
+            _buildMoviesList('Popular Movies', _popularMovies),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMoviesList(String title, List<Movie> movies) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              return Container(
+                width: 120,
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                color: Colors.grey[300],
+                child: Center(
+                  child: Text(movie.title, textAlign: TextAlign.center),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
